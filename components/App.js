f49ed4c16599c1acc0da1f678f6a85a18bb68728
Ryan Ashby <rjapsu160@gmail.com>
2022-05-08 12:33:56 -0700

    create and add base code to Headlines.js and App.js

---
+++ components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import Headlines from './Headlines';
+
+import React from 'react';
+import './App.css';
+import Headlines from './Headlines';
+
+function App() {
+  return (
+    <React.Fragment>
+      < Headlines />
+    </React.Fragment>
+  );
+}
+
+export default App;
